b5f51adc5b545174afb2a2ec09937f322a150e45
Fayaz ALI <92179108+1fayaz@users.noreply.github.com>
2024-11-13 20:32:45 +0500

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
 import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Contact from "./Contact/page";
 import Services from "./Services/page";
-import About from "./About/page";
+
 
 
 const Page= ()=>{
   return (
     <div>
-       
        <Services></Services>
+     
        
        
         
